76b16ac480c09be1a7ad25f4259884f19765076f
Caleb Cox <canac@users.noreply.github.com>
2026-08-26 10:18:49 -0500

    Support streaming

--- src/runtime.js
+++ src/runtime.js
@@ -18,3 +18,49 @@ Object.defineProperty(globalThis, 'args', {
 });
 
 globalThis.stdin = globalThis.s = lines();
+
+let writeLineOp = null;
+
+const writeLine = (value) => {
+  if (value !== null && value !== undefined) {
+    writeLineOp(`${value}`);
+  }
+};
+
+const isIterable = (result) => {
+  if (result === null || typeof result !== 'object' || result instanceof String) {
+    // Don't iterate strings
+    return false;
+  }
+  const method = result[Symbol.iterator];
+  if (typeof method === 'function') {
+    return true;
+  }
+  if (method === null || method === undefined) {
+    return false;
+  }
+  throw new TypeError(`${Deno.core.ops.op_detail_string(result)} is not iterable`);
+};
+
+Object.defineProperty(globalThis, '__kawWrite', {
+  value: (result) => {
+    writeLineOp ||= Deno.core.ops.op_write_line;
+
+    // Fast path for arrays because index iteration is faster than for-of loop
+    if (Array.isArray(result)) {
+      for (let index = 0; index < result.length; ++index) {
+        writeLine(result[index]);
+      }
+      return;
+    }
+
+    if (!isIterable(result)) {
+      writeLine(result);
+      return;
+    }
+
+    for (const value of result) {
+      writeLine(value);
+    }
+  }
+});
